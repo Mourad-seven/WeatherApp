7dc24ef2102b73327bf415bdf3a7a1e1a6b0b441
user <mourad.elbouaananii@gmail.com>
2018-02-05 15:27:08 +0100

    Add API "Wunderground"

--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -13,7 +13,7 @@ export class TabsPage {
   tab1Root = HomePage;
   tab2Root = AboutPage;
   tab3Root = ContactPage;
-  tab4root = SettingsPage;
+  settingsRoot = SettingsPage;
 
   constructor() {
 
